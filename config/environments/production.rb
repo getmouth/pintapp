2193abed89d19f4a8a95cbdd89cf419a845fc4fa
getmouth <emmopk11@outlook.com>
2018-01-05 00:19:32 +0100

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Added devise default url
+  config.action_mailer.default_url_options = { host: 'https://pintapp.herokuapp.com/'}
 end
