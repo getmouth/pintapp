381fcf6a1cf9435a861ea175637111f314e7a6a9
getmouth <emmopk11@outlook.com>
2018-01-04 17:03:40 +0100

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get  'home/index'
+  
+  get  'home', to: 'home#index'
+  get  'about',to: 'home#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
